f4dc712744e3c3daaacf1ff690062a3cb87a6b57
leconghoa11a1@gmail.com <hoa310119>
2022-06-23 16:49:17 +0700

    update code layout sidebar

---
+++ src/pages/Live/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function LIVE() {
+  return <h2>LIVE page</h2>;
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,12 +6,14 @@ import Home from '~/pages/Home';
 import Profile from '~/pages/Profile';
 import Search from '~/pages/Search';
 import Upload from '~/pages/Upload';
+import Live from '~/pages/Live';
 import config from '~/config';
 // Public routes
 const publicRoutes = [
   { path: config.routes.home, component: Home },
   { path: config.routes.following, component: Following },
   { path: config.routes.profile, component: Profile },
+  { path: config.routes.live, component: Live },
   { path: config.routes.upload, component: Upload, layout: HeaderOnly },
   { path: config.routes.search, component: Search, layout: null },
 ];
